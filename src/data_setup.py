bcde14180ad6b917e50a6cd0ff53f1dfe6b1da13
Sohambasu07 <soham.basu07@gmail.com>
2024-01-18 23:05:54 +0530

    Changed data_setup, added datasets.yaml and pre-downloaded datasets

--- src/data_setup.py
+++ src/data_setup.py
@@ -1,26 +1,44 @@
 import os
 from pathlib import Path
 import requests
+import yaml
+import argparse
 
-class Dataset_Setup:
-    def __init__(self, 
-                 data_dir: str, 
-                 url: str, 
-                 dataset_name: str, 
-                 root_dir: Path = Path(os.getcwd())):
-        self.root_dir = root_dir
-        self.data_dir = root_dir / data_dir
-        self.url = url
-        self.dataset_name = dataset_name
+def download_datasets(data_dir, 
+                      dataset_name, 
+                      url,
+                      endswith):
+    if (dataset_name + endswith) in os.listdir(data_dir):
+        print(f"Dataset {dataset_name} already downloaded!")
+        return
+    
+    print(f"Downloading dataset {dataset_name}...")
+    r = requests.get(url, verify = False)
+    with open(data_dir / (dataset_name + endswith), "wb") as f:
+        f.write(r.content)
+    print("Download complete.")
 
-    def download(self):
-        if os.path.exists(self.data_dir / self.dataset_name):
-            print("Dataset already downloaded!")
-            return
-        
-        print("Downloading dataset...")
-        r = requests.get(self.url)
-        endswith = self.url.split(".")[-1]
-        with open(self.data_dir / self.dataset_name + endswith, "wb") as f:
-            f.write(r.content)
-        print("Download complete.")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--root_dir", type=str, default=os.getcwd())
+    parser.add_argument("--dataset_cfg", type=str, default="configs/datasets.yaml")
+    parser.add_argument("--data_dir", type=str, default="data")
+
+    args = parser.parse_args()
+    root_dir = Path(args.root_dir)
+    data_dir = root_dir / args.data_dir
+    dataset_cfg = root_dir / args.dataset_cfg
+
+    with open(dataset_cfg, "r") as f:
+        dataset_config = yaml.safe_load(f)
+
+    # print(list(dataset_config.keys()))
+
+    for dataset in dataset_config:
+        print(dataset)
+        # print(dataset_config[dataset])
+        download_datasets(data_dir = data_dir,
+                          dataset_name = dataset,
+                          url = dataset_config[dataset]["url"],
+                          endswith = ".arff"
+                          )
